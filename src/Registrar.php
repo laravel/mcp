8301058af00003b0d86dc4ad2094c6a64ebe8f2f
Peter Suhm <peter@suhm.dk>
2025-05-21 13:31:35 +0200

    move SSE stuff to different branch

--- src/Registrar.php
+++ src/Registrar.php
@@ -5,7 +5,6 @@ namespace Laravel\Mcp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-use Laravel\Mcp\Transport\HttpSseTransport;
 use Laravel\Mcp\Transport\HttpStreamTransport;
 use Laravel\Mcp\Transport\StdioTransport;
 
@@ -17,25 +16,6 @@ class Registrar
 
         return Route::post($handle, function (Request $request) use ($server) {
             $transport = new HttpStreamTransport($request);
-            $server->connect($transport);
-
-            return $transport->run();
-        });
-    }
-
-    public function sse($handle, string $serverClass)
-    {
-        $server = new $serverClass();
-
-        Route::get($handle, function (Request $request) use ($server) {
-            $transport = new HttpSseTransport($request);
-            $server->connect($transport);
-
-            return $transport->run();
-        });
-
-        Route::post($handle.'/messages', function (Request $request) use ($server) {
-            $transport = new HttpSseTransport($request);
             $server->connect($transport);
 
             return $transport->run();
