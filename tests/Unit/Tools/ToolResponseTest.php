956e46f5b2009dbc830669c72d104b886f809abc
Peter Suhm <peter@suhm.dk>
2025-05-26 13:06:04 +0200

    add unit test for ToolResponse

---
+++ tests/Unit/Tools/ToolResponseTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laravel\Mcp\Tests\Unit\Tools;
+
+use Laravel\Mcp\Tools\ToolResponse;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class ToolResponseTest extends TestCase
+{
+    #[Test]
+    public function it_returns_a_valid_tool_response()
+    {
+        $responseText = 'This is a test response.';
+        $toolResponse = new ToolResponse($responseText);
+
+        $expectedArray = [
+            'content' => [
+                [
+                    'type' => 'text',
+                    'text' => $responseText,
+                ],
+            ],
+            'isError' => false,
+        ];
+
+        $this->assertSame($expectedArray, $toolResponse->toArray());
+    }
+}
