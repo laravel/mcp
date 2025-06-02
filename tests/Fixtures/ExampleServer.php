4d453d3b622f1592fecefa714fa78dd0fff4efb9
Peter Suhm <peter@suhm.dk>
2025-06-02 10:43:47 +0200

    fix McpServerTest

--- tests/Fixtures/ExampleServer.php
+++ tests/Fixtures/ExampleServer.php
@@ -3,10 +3,13 @@
 namespace Laravel\Mcp\Tests\Fixtures;
 
 use Laravel\Mcp\Server;
+use Laravel\Mcp\Tests\Fixtures\ExampleTool;
+use Laravel\Mcp\Tests\Fixtures\StreamingTool;
 
 class ExampleServer extends Server
 {
     public array $tools = [
         'hello-tool' => ExampleTool::class,
+        'streaming-tool' => StreamingTool::class,
     ];
 }
--- tests/Fixtures/InitializedServer.php
+++ tests/Fixtures/InitializedServer.php
@@ -4,11 +4,14 @@ namespace Laravel\Mcp\Tests\Fixtures;
 
 use Laravel\Mcp\Server;
 use Laravel\Mcp\SessionContext;
+use Laravel\Mcp\Tests\Fixtures\ExampleTool;
+use Laravel\Mcp\Tests\Fixtures\StreamingTool;
 
 class InitializedServer extends Server
 {
     public array $tools = [
         'hello-tool' => ExampleTool::class,
+        'streaming-tool' => StreamingTool::class,
     ];
 
     public function boot($clientCapabilities = [])
