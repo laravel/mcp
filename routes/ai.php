36b66da94bf4ab262c1b9fdedf9b2638991a0eb5
Peter Suhm <peter@suhm.dk>
2025-06-16 14:06:46 +0200

    update ai.php

--- routes/ai.php
+++ routes/ai.php
@@ -1,5 +1,5 @@
 <?php
 use Laravel\Mcp\Facades\Mcp;
 
-Mcp::web('demo', \App\McpServers\ExampleServer::class);
-Mcp::cli('demo', \App\McpServers\ExampleServer::class);
+Mcp::web('demo', \App\McpServers\PublicServer::class);
+Mcp::local('demo', \App\McpServers\LocalServer::class);
