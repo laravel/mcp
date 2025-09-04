f0986407f734d80cb205b98a66ad88c5f90d0347
Nuno Maduro <enunomaduro@gmail.com>
2025-09-04 16:59:16 +0100

    import

--- src/Facades/Mcp.php
+++ src/Facades/Mcp.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Laravel\Mcp\Facades;
 
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static void local(string $handle, string $serverClass)
- * @method static \Illuminate\Routing\Route web(string $handle, string $serverClass)
+ * @method static Route web(string $handle, string $serverClass)
  * @method static callable|null getLocalServer(string $handle)
  * @method static string|null getWebServer(string $handle)
  *
