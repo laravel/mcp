55bde8a6524d8807cf0fe01ce9109e003d274034
Nuno Maduro <enunomaduro@gmail.com>
2025-09-08 22:01:52 +0100

    lowercase test methods

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,27 +1,27 @@
 <?php
 
-arch('Strict and safe')
+arch('strict and safe')
     ->expect('Laravel\Mcp')
     ->toUseStrictTypes()
     ->not->toUse(['die', 'dd', 'dump', 'var_dump']);
 
-arch('MCP methods extend base class')
+arch('mcp methods extend base class')
     ->expect('Laravel\Mcp\Server\Methods')
     ->toOnlyImplement('Laravel\Mcp\Server\Contracts\Method');
 
-arch('Tool annotations implement annotation interface')
+arch('tool annotations implement annotation interface')
     ->expect('Laravel\Mcp\Server\Tools\Annotations')
     ->toOnlyImplement('Laravel\Mcp\Server\Contracts\Tools\Annotation');
 
-arch('Contracts are interfaces')
+arch('contracts are interfaces')
     ->expect('Laravel\Mcp\Server\Contracts\*')
     ->toBeInterfaces();
 
-arch('Exceptions extend')
+arch('exceptions extend')
     ->expect('Laravel\Mcp\Server\Exceptions')
     ->toExtend(Exception::class);
 
-arch('Commands extend command')
+arch('commands extend command')
     ->expect('Laravel\Mcp\Console\Commands')
     ->toExtend(\Illuminate\Console\Command::class)
     ->toHaveAttribute(\Symfony\Component\Console\Attribute\AsCommand::class);
