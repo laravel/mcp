2dc6c0e56c63b56ea2bd6bb0f26fb91d9c4a2376
ashleyhindle <454975+ashleyhindle@users.noreply.github.com>
2025-09-08 16:10:30 +0000

    Fix code styling

--- src/Server/Registrar.php
+++ src/Server/Registrar.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Laravel\Mcp\Server;
 
-use Exception;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Route as Router;
 use Illuminate\Support\Str;
