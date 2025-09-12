fb704f4e32ebda079dfd6250b35fbd8613b53964
Ashley Hindle <ashley.hindle@laravel.com>
2025-09-12 12:02:27 +0100

    feat: add 'user()' to request

--- src/Request.php
+++ src/Request.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Laravel\Mcp;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Traits\InteractsWithData;
@@ -71,5 +72,10 @@ class Request implements Arrayable
     public function validate(array $rules, array $messages = [], array $attributes = []): array
     {
         return Validator::validate($this->all(), $rules, $messages, $attributes);
+    }
+
+    public function user(): ?Authenticatable
+    {
+        return call_user_func(app('auth')->userResolver(), null);
     }
 }
