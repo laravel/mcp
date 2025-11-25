2ad7723443f976e67916296feb9d2ddc48c356ab
Pushpak Chhajed <pushpak1300@gmail.com>
2025-11-25 17:58:10 +0530

    Add Test

---
+++ tests/Feature/Console/MakeResourceTemplateCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+it('can create a resource template class', function (): void {
+    $response = $this->artisan('make:mcp-resource-template', [
+        'name' => 'TestResourceTemplate',
+    ]);
+
+    $response->assertExitCode(0)->run();
+
+    $this->assertFileExists(app_path('Mcp/Resources/TestResourceTemplate.php'));
+});
+
+it('may publish a custom stub', function (): void {
+    $this->artisan('vendor:publish', [
+        '--tag' => 'mcp-stubs',
+        '--force' => true,
+    ])->assertExitCode(0)->run();
+
+    $this->assertFileExists(base_path('stubs/resource-template.stub'));
+});
