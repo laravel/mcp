5f88c191185301f582640bc2e2c0cc5bc538a4e7
rupeshstha <rupeshshrestha9818@gmail.com>
2026-04-16 13:47:47 +1200

    fix: fix lint issue

--- src/Facades/Mcp.php
+++ src/Facades/Mcp.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace Laravel\Mcp\Facades;
 
-use Illuminate\Support\Facades\Facade;
+use Laravel\Mcp\Server;
+use Illuminate\Routing\Route;
 use Laravel\Mcp\Server\Registrar;
+use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Illuminate\Routing\Route web(string $route, string<\Laravel\Mcp\Server> $serverClass)
- * @method static void local(string $handle, string<\Laravel\Mcp\Server> $serverClass)
+ * @method static Route web(string $route, class-string<Server> $serverClass)
+ * @method static void local(string $handle, class-string<Server> $serverClass)
  * @method static callable|null getLocalServer(string $handle)
- * @method static \Illuminate\Routing\Route|null getWebServer(string $route)
- * @method static array<string, callable|\Illuminate\Routing\Route> servers()
+ * @method static Route|null getWebServer(string $route)
+ * @method static array<string, callable|Route> servers()
  * @method static void oauthRoutes(string $oauthPrefix = 'oauth')
  * @method static array<string, string> ensureMcpScope()
  *
- * @see \Laravel\Mcp\Server\Registrar
+ * @see Registrar
  */
 class Mcp extends Facade
 {
