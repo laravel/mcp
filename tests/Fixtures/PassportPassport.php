4f59eb551ab23740add0b616982fb40bda66b90d
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2026-09-09 05:54:15 -0700

    Add oauth client logo and uri (#337)
    * Add oauth client logo and uri
    
    * fix test
    
    * fix rector
    
    * Simplify client metadata persistence and render it in the authorize view
    
    * Guard authorize view against strict mode and test rendered client metadata
    
    * Handle null app metadata domains correctly
    
    ---------
    
    Co-authored-by: Pushpak Chhajed <pushpak1300@gmail.com>
    (cherry picked from commit cbd8ca4f2fa360763050c4f71c10fc7f553a04be)

--- tests/Fixtures/PassportPassport.php
+++ tests/Fixtures/PassportPassport.php
@@ -8,8 +8,20 @@ class Passport
 {
     public static $scopes = [];
 
+    public static $clientModel = Client::class;
+
     public static function tokensCan($scopes): void
     {
         self::$scopes = $scopes;
+    }
+
+    public static function useClientModel(string $clientModel): void
+    {
+        static::$clientModel = $clientModel;
+    }
+
+    public static function client(): Client
+    {
+        return new static::$clientModel;
     }
 }
